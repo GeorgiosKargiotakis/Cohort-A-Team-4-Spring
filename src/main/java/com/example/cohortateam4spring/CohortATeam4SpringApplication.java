1d102869a89f1e46504e78d298ea9983158efedf
GeorgiosKargiotakis <georgioskargiotakis@gmail.com>
2022-04-19 09:52:09 +0300

    project start

---
+++ src/main/java/com/example/cohortateam4spring/CohortATeam4SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.cohortateam4spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohortATeam4SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CohortATeam4SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cohortateam4spring/CohortATeam4SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cohortateam4spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohortATeam4SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
